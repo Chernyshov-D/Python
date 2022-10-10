2d2a45b59248170c7cf886d04f45b6f1a417fbb2
Chernyshov-D <imchd@yandex.ru>
2022-10-10 19:13:01 +0400

    Lesson 4

---
+++ Lesson_4/4_1.py
@@ -0,0 +1,10 @@
+# Вычислить число c заданной точностью d
+
+import math
+
+def pi(num):
+    pi = math.pi
+    return str(pi)[0:num + 2]
+
+num = int(input('Введите количество чисел после запятой: '))
+print(f'Число π = {pi(num)}')
